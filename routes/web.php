201177f514be771870f611490577d9598506a5dd
SportShopVN <tuantrungpham008@gmail.com>
2026-06-23 00:26:10 +0700

    Giao diện user

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
-use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
 Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-Route::post('/login', [RegisteredUserController::class, 'store'])->name('login.store');
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+Route::post('/logout', function (Request $request) {
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('/');
+})->name('logout');
