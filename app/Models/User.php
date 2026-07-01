7a2161274329f2f89c0d73195fcb12fbfaa4fcae
quanngoc07 <quanlai200@gmail.com>
2026-07-01 13:31:26 +0700

    hi

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,12 +19,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-    'name',
-    'email',
-    'phone',
-    'password',
-    'role',
-    'status',
+
     ];
 
     /**
