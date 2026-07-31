8f6db3cfc07c1f77f2952fc920e3b944210616c4
dungx3825 <dungphph52508@gmail.com>
2026-08-01 02:16:53 +0700

    giao diện  trang dashboard

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -608,7 +608,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    @stack('scripts')
+   
 
     <script>
     const sidebarToggle = document.getElementById('sidebarToggle');
@@ -642,6 +642,9 @@
 
     });
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+     @stack('scripts')
 </body>
 
 </html>
